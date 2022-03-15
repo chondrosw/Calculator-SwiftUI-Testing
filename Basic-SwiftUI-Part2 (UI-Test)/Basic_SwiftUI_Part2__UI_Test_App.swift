7bc637ae7a1e2c410c367e2f28fa0ad957cbd9fa
ChondroSW <chondrosatriowibowo@gmail.com>
2022-03-15 13:47:01 +0700

    Initial Commit

---
+++ Basic-SwiftUI-Part2 (UI-Test)/Basic_SwiftUI_Part2__UI_Test_App.swift
@@ -0,0 +1,20 @@
+//
+//  Basic_SwiftUI_Part2__UI_Test_App.swift
+//  Basic-SwiftUI-Part2 (UI-Test)
+//
+//  Created by Chondro Satrio Wibowo on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Basic_SwiftUI_Part2__UI_Test_App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
